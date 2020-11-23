e7ec06506b77a7b0beceb646ba0200cf524bbb84
Tsimur Charniauski <timurusus@gmail.com>
2020-11-23 22:38:39 +0300

    move functions from processing to main.py and create functions to load values to db

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sqlite3
 from sqlite3 import Error
 import shutil
-import os
-import re
 import glob
+from xml.dom import minidom
+
 """
 PLAN:
 create connection -> connect to db -> create tables if not exists
 get files from input folder to list -> parse it to variables -> variables load to db into appropriate tables 
 
 """
+
 #TODO: Logging
 #TODO: parse file into variables:
 #                   book_name,
@@ -22,8 +23,60 @@ get files from input folder to list -> parse it to variables -> variables load t
 #                   word, count, count_uppercase
 
 
-file_template = "input/*.txt"
+# file = "input/*.fb2"
+file = "input/Example.fb2"
 path_for_incorrect_files = "incorrect_input"
+list_of_files = []
+values_for_common_files_info_table = dict()
+
+
+def get_book_name_from_fb2(path_to_file):
+    xml_doc = minidom.parse(path_to_file)
+    book_name = xml_doc.getElementsByTagName("book-title")[0].firstChild.data
+    return book_name
+
+
+def get_number_of_paragraphs_from_fb2(path_to_file):
+    xml_doc = minidom.parse(path_to_file)
+    tags = xml_doc.getElementsByTagName("p")
+    return len(tags)
+
+
+def get_number_of_words_from_fb2(path_to_file):
+    return len(return_list_of_words(path_to_file=path_to_file))
+
+
+def return_list_of_words(path_to_file):
+    xml_doc = minidom.parse(path_to_file)
+    tags = xml_doc.getElementsByTagName("p")
+    list_of_words = []
+    for p in tags:
+        for i in p.childNodes:
+            list_of_words = list_of_words + (str(i.nodeValue).split())
+    return list_of_words
+
+
+def return_number_of_letters(path_to_file):
+    count = 0
+    for word in return_list_of_words(path_to_file=path_to_file):
+        count += len(word)
+    return count
+
+
+def return_number_of_words_with_lowercase(path_to_file):
+    count = 0
+    for word in return_list_of_words(path_to_file=path_to_file):
+        if word.islower():
+            count += 1
+    return count
+
+
+def return_number_of_words_with_capital_letters(path_to_file):
+    count = 0
+    for word in return_list_of_words(path_to_file=path_to_file):
+        if any(x.isupper() for x in word):
+            count += 1
+    return count
 
 
 def create_connection(db_file):
@@ -55,18 +108,61 @@ def main():
                                     number_of_letters int,
                                     words_with_capital_letters int,
                                     words_in_lowercase int);
-    """
+                                    """
+    sql_insert_into_common_table_info = "INSERT INTO common_files_info VALUES (?,?,?,?,?,?)"
+
+    conn = create_connection(database)
+    create_table(conn=conn, create_statement_sql=sql_create_table_common)
+    conn.commit()
+    get_values_for_common_files_info_table(file_temp=file)
+    conn.execute(sql_insert_into_common_table_info, (values_for_common_files_info_table["book_name"],
+                                                     values_for_common_files_info_table["number_of_paragraphs"],
+                                                     values_for_common_files_info_table["number_of_words"],
+                                                     values_for_common_files_info_table["number_of_letters"],
+                                                     values_for_common_files_info_table["words_with_capital_letters"],
+                                                     values_for_common_files_info_table["words_in_lowercase"]))
+    conn.commit()
+    conn.close()
+
+
+
     sql_create_table_for_each_file = """ 
     """
 
 
+def get_file_names_to_list(file_temp):
+    for file_name in glob.glob(file_temp):
+        list_of_files.append(file_name)
+    return list_of_files
 
-list_of_files = []
-for file in glob.glob(file_template):
-    list_of_files.append(file)
+
+def move_files(files, path_for_incorrect):
+    for file in files:
+        shutil.move(file, path_for_incorrect)
+
+
+def get_values_for_common_files_info_table(file_temp):
+    book_name = get_book_name_from_fb2(file_temp)
+    number_of_paragraphs = get_number_of_paragraphs_from_fb2(file_temp)
+    number_of_words = get_number_of_words_from_fb2(file_temp)
+    number_of_letters = return_number_of_letters(file_temp)
+    words_with_capital_letters = return_number_of_words_with_capital_letters(file_temp)
+    words_in_lowercase = return_number_of_words_with_lowercase(file_temp)
+    values_for_common_files_info_table['book_name'] = book_name
+    values_for_common_files_info_table['number_of_paragraphs'] = number_of_paragraphs
+    values_for_common_files_info_table['number_of_words'] = number_of_words
+    values_for_common_files_info_table['number_of_letters'] = number_of_letters
+    values_for_common_files_info_table['words_with_capital_letters'] = words_with_capital_letters
+    values_for_common_files_info_table['words_in_lowercase'] = words_in_lowercase
+
+
+
 
 
 if __name__ == '__main__':
-    print(list_of_files)
-    for file in list_of_files:
-        shutil.move(file, path_for_incorrect_files)
+    # main()
+    conn = create_connection("monitorapp.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM common_files_info")
+    res = cur.fetchall()
+    print(res)
--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
 # import xml.etree.ElementTree as ET
-from xml.dom import minidom
+
 
 # tree = ET.parse('Example.fb2')
 # root = tree.getroot()
@@ -9,57 +9,9 @@ from xml.dom import minidom
 #'Example.fb2'
 
 
-def get_book_name_from_fb2(path_to_file):
-    xml_doc = minidom.parse(path_to_file)
-    book_name = xml_doc.getElementsByTagName("book-title")[0].firstChild.data
-    return book_name
-
-
-def get_number_of_paragraphs_from_fb2(path_to_file):
-    xml_doc = minidom.parse(path_to_file)
-    tags = xml_doc.getElementsByTagName("p")
-    return len(tags)
-
-
-def get_number_of_words_from_fb2(path_to_file):
-    return len(return_list_of_words(path_to_file=path_to_file))
-
-
-def return_list_of_words(path_to_file):
-    xml_doc = minidom.parse(path_to_file)
-    tags = xml_doc.getElementsByTagName("p")
-    list_of_words = []
-    for p in tags:
-        for i in p.childNodes:
-            list_of_words = list_of_words + (str(i.nodeValue).split())
-    return list_of_words
-
-
-def return_number_of_letters(path_to_file):
-    count = 0
-    for word in return_list_of_words(path_to_file=path_to_file):
-        count += len(word)
-    return count
-
-
-def return_number_of_words_with_lowercase(path_to_file):
-    count = 0
-    for word in return_list_of_words(path_to_file=path_to_file):
-        if word.islower():
-            count += 1
-    return count
-
-
-def return_number_of_words_with_capital_letters(path_to_file):
-    count = 0
-    for word in return_list_of_words(path_to_file=path_to_file):
-        if any(x.isupper() for x in word):
-            count += 1
-    return count
-
 
 
 # print(get_number_of_words_from_fb2('Example.fb2'))
 
-print(return_number_of_words_with_capital_letters('Example.fb2'))
+#print(return_number_of_words_with_capital_letters('Example.fb2'))
 
